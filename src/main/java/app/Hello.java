298ed1e6d4ebd660f9d5b43f55f0d2a825b29fea
Marcio Hernani <marcio.hernani@gmail.com>
2025-08-26 01:34:07 +0000

    Aula 25/08/2025 22h

--- src/main/java/app/Hello.java
+++ src/main/java/app/Hello.java
@@ -7,8 +7,25 @@ public class Hello {
 
         Genero g2 = new Genero(2, "");
 
-        System.out.println(g.getNome());
-        System.out.println(g2.getId());
+        //System.out.println(g.getNome());
+        //System.out.println(g2.getId());
+
+        Genero policial = new Genero(1, "Policial");
+        Genero aventura = new Genero(2, "Aventura");
+        Genero terror = new Genero(3, "Terror");
+
+        Filme f1 = new Filme();
+        f1.setId(1);
+        f1.setTitulo("A Sociedade do Anel");
+        f1.setGenero(aventura);
+
+        Filme f2 = new Filme(2, "Duna", aventura);
+
+        Filme f3 = new Filme(3, "Entre facas e segredos", policial);
+
+        System.out.println(f1.getTitulo());
+        System.out.println(f2.getTitulo());
+        System.out.println(f3.toString());
     }
 }
 
